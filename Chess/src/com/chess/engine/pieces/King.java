b1b312268496b67fc518e9691f649813b5bbbfd3
Winnie Liang <wliang6@buffalo.edu>
2016-08-28 16:37:50 -0400

    Finished basis of King class

---
+++ Chess/src/com/chess/engine/pieces/King.java
@@ -0,0 +1,71 @@
+package com.chess.engine.pieces;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.chess.engine.board.Board;
+import com.chess.engine.board.BoardUtils;
+import com.chess.engine.board.Move;
+import com.chess.engine.board.Tile;
+import com.google.common.collect.ImmutableList;
+
+/**
+ * Chess
+ * @author Winnie Aug 28, 2016
+ * King.java
+ */
+public class King extends Piece {
+
+	private final static int[] CANDIDATE_MOVE_COORDINATES = {-9, -8, -7, -1, 1, 7, 8, 9};
+	
+	/**
+	 * @param piecePosition
+	 * @param pieceAlliance
+	 */
+	King(int piecePosition, Alliance pieceAlliance) {
+		super(piecePosition, pieceAlliance);
+	}
+
+
+	@Override
+	public Collection<Move> calculateLegalMoves(final Board board) {
+		final List<Move> legalMoves = new ArrayList<>();
+		for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATES){
+			final int candidateDestinationCoordinate = this.piecePosition + currentCandidateOffset;
+			
+			//Edge cases
+			if(isFirstColumnExclusion(this.piecePosition, currentCandidateOffset) || 
+					isEighthColumnExclusion(this.piecePosition, currentCandidateOffset)){
+				continue;
+			}
+			
+			if(BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)){
+				final Tile candidateDestinationTile = board.getTile(candidateDestinationCoordinate);
+				if(!candidateDestinationTile.isTileOccupied()){
+					legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate)); 			
+				}else{ //if it is on an occupied tile, get the piece on that tile and alliance (B or W)
+					final Piece pieceAtDestination = candidateDestinationTile.getPiece();
+					final Alliance pieceAlliance = pieceAtDestination.getPieceAlliance(); //get alliance of piece that is occupying tile
+					if(this.pieceAlliance != pieceAlliance){ //if alliance is not equal to current knight's alliance
+						legalMoves.add(new Move.AttackMove(board, this, candidateDestinationCoordinate, pieceAtDestination));
+					}
+				}
+			}
+			
+		}
+		
+		return ImmutableList.copyOf(legalMoves);
+	}
+	
+	//Edge case methods
+	private static boolean isFirstColumnExclusion(final int currentPosition, final int candidateOffset){
+		return BoardUtils.FIRST_COLUMN[currentPosition] && (candidateOffset == -9 || candidateOffset == -1 || candidateOffset == 7); 
+	}
+	
+	private static boolean isEighthColumnExclusion(final int currentPosition, final int candidateOffset){
+		return BoardUtils.EIGHTH_COLUMN[currentPosition] && (candidateOffset == -7 || candidateOffset == 1 || candidateOffset == 9);
+	}
+	
+
+}
